83f0b4c034d50efa35460c78e271aa0303830781
ashish6566 <aacsspkt3@users.noreply.github.com>
2019-09-06 07:40:34 +0545

    Update App.vue

--- app-1.0.0/src/App.vue
+++ app-1.0.0/src/App.vue
@@ -8,7 +8,14 @@
 </template>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
+  background-color: aliceblue;
+  height: 100vh;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
